97c66f24181361cb73bf95a6430410b5084361ee
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-23 00:03:45 +0000

    Solved Part C, Iteration 3

--- UserInterface.hs
+++ UserInterface.hs
@@ -17,18 +17,41 @@ getGuess count list = do
     input <- getChar'
     putChar ' ' 
     do
-        case checkGuessChar input list of
-            True -> do
-                        remaining <- getGuess (count-1) list
-                        return (toLower input: remaining)
-            False -> do
-                        putChar '\b'
-                        putChar '\b'
-                        remaining <- getGuess (count) list
-                        return remaining
+        case input of
+            '.' -> do
+                    putChar '\b'
+                    putChar '\b'
+                    putChar '\b'
+                    putChar '\b'
+
+                    remaining <- getGuess (count+1) list
+                    return ('\b': remaining)
+            _ -> do
+                case checkGuessChar input list of
+                    True -> do
+                                remaining <- getGuess (count-1) list
+                                if(backspaceLast remaining) then
+                                    return (removeFirst remaining) 
+                                else
+                                    return (toLower input: remaining)
+                    False -> do
+                                putChar '\b'
+                                putChar '\b'
+                                remaining <- getGuess (count) list
+                                return remaining
+
+removeFirst :: [a] -> [a]
+removeFirst [] = []
+removeFirst (x:xs) = xs
+
+backspaceLast :: [Char] -> Bool
+backspaceLast ('\b': list) = True
+backspaceLast _ = False
 
 checkGuessChar :: Char -> String -> Bool 
 checkGuessChar c "" = False 
 checkGuessChar c (elem: string) = c==elem || checkGuessChar c string
+
+
 
 -- Part D - gameplay
