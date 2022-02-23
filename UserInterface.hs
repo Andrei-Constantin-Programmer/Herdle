02bad2dc2d84986ef5ec3ceeef426b096e440d3c
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-23 01:37:32 +0000

    Extract part E into new file Main.hs

--- UserInterface.hs
+++ UserInterface.hs
@@ -8,10 +8,6 @@ import Data.Char
 import Data.Binary.Get (remaining)
 import GHC.IO.IOMode (IOMode(ReadMode))
 import Control.Exception (handle)
-import Data.Time (getCurrentTime)
-import Text.XHtml (nowrap)
-import Data.Time.Clock
-import Data.Time.Calendar
 
 -- Part C - receiving guesses
 
@@ -106,24 +102,3 @@ winningCondition (status:statuses) = (status==Here) && winningCondition statuses
 
 go :: String -> IO()
 go word = loop (map toLower word) "abcdefghijklmnopqrstuvwxyz" 6 1
-
--- Part E - stand-alone game, word list
-
-readWords :: FilePath -> IO [String]
-readWords = fmap lines . readFile
-
-getCurrentDay :: IO Int
-getCurrentDay = do
-    now <- getCurrentTime
-    let (year, month, day) = toGregorian $ utctDay now
-    return day
-    
-getWordForDay :: [String] -> Int -> String 
-getWordForDay words day = words!!day
-
-main :: IO()
-main = do 
-    words <- readWords "wordlist.txt"
-    day <- getCurrentDay 
-    let word = getWordForDay words (day-1)
-    go word
