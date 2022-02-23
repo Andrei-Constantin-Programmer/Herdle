e5120c53d0b37d19c9f4c9cba2c4b27932812d4d
Andrei-Constantin-Programmer <55529876+Andrei-Constantin-Programmer@users.noreply.github.com>
2022-02-23 01:41:11 +0000

    Added extra comments

--- Main.hs
+++ Main.hs
@@ -16,22 +16,25 @@ import Data.Time.Calendar
 
 -- Part E - stand-alone game, word list
 
+-- Read the words from the file
 readWords :: FilePath -> IO [String]
 readWords = fmap lines . readFile
 
+-- Get the current day (1 to 30)
 getCurrentDay :: IO Int
 getCurrentDay = do
     now <- getCurrentTime
     let (year, month, day) = toGregorian $ utctDay now
     return day
     
+-- Get the corresponding word from the list for the given day
 getWordForDay :: [String] -> Int -> String 
 getWordForDay words day = words!!day
 
+-- Play the game
 main :: IO()
 main = do 
     words <- readWords "wordlist.txt"
     day <- getCurrentDay 
     let word = getWordForDay words (day-1)
     go word
-    
--- UserInterface.hs
+++ UserInterface.hs
@@ -15,6 +15,7 @@ import Control.Exception (handle)
 getGuess :: Int -> String -> IO String 
 getGuess max list = getGuessMax max max list
 
+-- Get guess with a known number of max characters
 getGuessMax :: Int -> Int -> String -> IO String 
 getGuessMax _ 0 _ = do
     putChar '\n'
@@ -52,15 +53,18 @@ getGuessMax max count list = do
                                 remaining <- getGuessMax max (count) list
                                 return remaining
 
+-- Remove the first element of the given list
 removeFirst :: [a] -> [a]
 removeFirst [] = []
 removeFirst (x:xs) = xs
 
+-- Check if the first element of the list is a backspace 
 backspaceLast :: [Char] -> Bool
 backspaceLast (_: []) = False
 backspaceLast ('\b': list) = True
 backspaceLast _ = False
 
+-- Check if the given char is part of the given string
 checkGuessChar :: Char -> String -> Bool 
 checkGuessChar c "" = False 
 checkGuessChar c (elem: string) = c==elem || checkGuessChar c string
@@ -68,6 +72,8 @@ checkGuessChar c (elem: string) = c==elem || checkGuessChar c string
 
 
 -- Part D - gameplay
+
+-- The gameplay loop
 loop :: String -> [Char] -> Int -> Int -> IO()
 loop word available maxAttempts attempts =
     do
@@ -91,14 +97,16 @@ loop word available maxAttempts attempts =
                         else
                             loop word (updateAvailable available (checkGuess guess word)) maxAttempts (attempts+1)
 
-
+-- Get the list of statuses from the list of char-status touples
 getStatuses :: [(Char, Status)] -> [Status]
 getStatuses [] = []
 getStatuses ((char, status): statuses) = status: getStatuses statuses
 
+-- Check if the winning condition has been met
 winningCondition :: [Status] -> Bool
 winningCondition [] = True 
 winningCondition (status:statuses) = (status==Here) && winningCondition statuses
 
+-- Run the game with the given word
 go :: String -> IO()
 go word = loop (map toLower word) "abcdefghijklmnopqrstuvwxyz" 6 1
